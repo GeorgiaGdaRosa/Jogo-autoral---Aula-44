5882bb9819ec7fa1dcc3bcea91b8348f826e0346
GeorgiaGdaRosa <georgiagdr03@gmail.com>
2022-11-10 15:04:55 -0300

    jogo autoral

--- sketch.js
+++ sketch.js
@@ -1,5 +1,12 @@
 var bg, backgroundImg
 var girl, girlBlink, girlWalk
+var solo
+var plataformaPosition=[
+   {x:320,y:250},
+   {x:500,y:230},
+   {x:1000,y:250}
+]
+var plataformas 
 
 function preload() {
    backgroundImg = loadImage("./assets/background2.png")
@@ -15,11 +22,18 @@ function setup() {
    girlBlink.frameDelay = 10
    girl.addAnimation("girlBlink", girlBlink)
    girl.addAnimation("walk", girlWalk)
+   solo = createSprite(width/2,height-10,width,10)
+   solo.visible = false
+   plataformas = new Group()
+   for(var i = 0;i<plataformaPosition.length;i++){
+      var plataforma = createSprite(plataformaPosition[i].x,plataformaPosition[i].y,50,20)
+      plataformas.add(plataforma)
+   }
 }
 
 function draw() {
    background(220)
-
+   //girl.collide(plataformas)
 
    if (bg.x < -350) {
       bg.x = bg.width / 2
@@ -29,25 +43,47 @@ function draw() {
 
    drawSprites()
    text("x:" + mouseX + "y:" + mouseY, mouseX, mouseY)
+   
 
 }
 
 function playerControls() {
    bg.velocityX = 0
-   
+   plataformas.setVelocityXEach(0)
+   //girl.collide(solo)
+   girl.velocityY+= 0.8
    girl.changeAnimation("girlBlink")
    if (keyIsDown(RIGHT_ARROW)) {
       girl.changeAnimation("walk", girlWalk)
+      girl.mirrorX(1)
       bg.velocityX = -1
-      girl.x += 2
+      plataformas.setVelocityXEach(-1)
+      girl.x += 1.2
    }
 
    if (keyIsDown(LEFT_ARROW)) {
       girl.changeAnimation("walk", girlWalk)
-      girl.x -= 2
+      girl.mirrorX(-1)
+      girl.x -= 1.2
       bg.velocityX = 1
+      plataformas.setVelocityXEach(1)
    }
 
+  // if (keyDown("space") && girl.y>=240){
+     // girl.velocityY = -10
+   //}
+
+   if(girl.collide(plataformas)){
+      if (keyDown("space")){
+         girl.velocityY = -10
+      }
+   }
+
+   if(girl.collide(solo)){
+      if (keyDown("space") && girl.y>=240){
+         girl.velocityY = -10
+      }
+   }
 }
 
 
